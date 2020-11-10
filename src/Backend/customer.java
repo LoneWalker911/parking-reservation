d2412cd5f59a0876993e863b8279287d946c7853
LoneWalker911 <59104939+LoneWalker911@users.noreply.github.com>
2020-11-10 21:59:11 +0530

    Customer

--- src/Backend/customer.java
+++ src/Backend/customer.java
@@ -6,6 +6,7 @@
 package Backend;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,31 +16,83 @@ import java.sql.Statement;
  * @author Prashan
  */
 public class customer {
-    private int cus_id;
-    private String cus_name;
-    private String cus_mobile;
-    private String cus_email;
-    private String cus_address;
+    private int cus_id = 0;
+    private String cus_name = null;
+    private String cus_mobile = null;
+    private String cus_email = "";
+    private String cus_address = "";
+    private String username = null;
+    private String password = null;
+    EventLog log = new EventLog();
     dbConnection db = new dbConnection();
     private final Connection con = db.CreateConn();
 
+    public String addCustomer()
+    {
+        if(cus_name!=null && cus_mobile != null && getPassword() != null)
+        {
+        try
+        {
+            String query = "INSERT INTO customer (name, password, mobile, address, email) "
+                         + " values (?, ?, ?, ?, ?)";
+
+            //using a prepared statement to preven SQL Injection and other simillar attacks
+            PreparedStatement prest = con.prepareStatement(query);
+            prest.setString (1, getCus_name());
+            prest.setString (2, getPassword());
+            prest.setString (3, getCus_mobile());
+            prest.setString (4, getCus_address());
+            prest.setString (5, getCus_email());
+
+            // execute the preparedstatement
+            prest.execute();
+            log.Write("Customer_ID : " + getLastid() + " added to customer table.");
+            String ret = "Customer ID : " + getLastid() + "\nPassword : " + getPassword();
+            return ret;
+        }
+        catch (SQLException e)
+            {
+              System.err.println("Got an exception!");
+              System.err.println(e.getMessage());
+              return "Process Failed, Contact admin";
+            }
+        }
+        else return "Enter requied information";
+    }
+    
+    public ResultSet getCustomers()
+    {
+        try
+        {
+            int id = getLastid();
+            String query = "SELECT * FROM customer";
+
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            return rs;
+
+        }
+        catch (SQLException e)
+            {
+              System.err.println("Exception occured!");
+              return null;
+            }
+    }
     
     public int getLastid() throws SQLException
     {
         int id = -1;
         
-        String sql = "SELECT id FROM roles WHERE id = (SELECT MAX(id) FROM roles)";
+        String sql = "SELECT id FROM customer WHERE id = (SELECT MAX(id) FROM customer)";
         
-        Statement st = con.createStatement();
-        
-        ResultSet rs = st.executeQuery(sql);
-        
-        while (rs.next())
-      {
-        id = rs.getInt("id");       
-      }
-      st.close();
-      con.close();
+        try (Statement st = con.createStatement()) {
+            ResultSet rs = st.executeQuery(sql);
+            
+            while (rs.next())
+            {
+                id = rs.getInt("id");
+            } }
       
       return id;
     }
@@ -112,6 +165,20 @@ public class customer {
      */
     public void setCus_address(String cus_address) {
         this.cus_address = cus_address;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
     
     
